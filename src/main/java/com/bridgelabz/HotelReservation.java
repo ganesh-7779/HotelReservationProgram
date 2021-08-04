774af05849031d357eb3def5f3948f547b43d391
Ganesh <ganeshgavhad9@gmail.com>
2021-08-04 08:35:39 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation Program");
+    }
+}
